803d68dd2288c0987454702f31c029ea450ca1d5
EC2 Default User <sjm34@cornell.edu>
2016-04-05 18:55:03 +0000

    made conditional changes to getLC

--- getLC.rb
+++ getLC.rb
@@ -16,27 +16,57 @@ def getLC(lcName, region, credentials)
       launch_configuration_names: [lcName]})
    lc = lcd.launch_configurations[0]
 
-   opt = {
-      launch_configuration_name: lcName,
-      image_id: lc.image_id ,
-      key_name: lc.key_name ,
-      security_groups: lc.security_groups ,
-      classic_link_vpc_id: lc.classic_link_vpc_id ,
-      classic_link_vpc_security_groups: lc.classic_link_vpc_security_groups ,
-      user_data: lc.user_data,
-      instance_type: lc.instance_type,
-      kernel_id: lc.kernel_id,
-      ramdisk_id: lc.ramdisk_id,
-      block_device_mappings: lc.block_device_mappings,
-      instance_monitoring: {
-        enabled: lc.instance_monitoring.enabled,
-      },
-      spot_price: lc.spot_price,
-      iam_instance_profile: lc.iam_instance_profile,
-      ebs_optimized: lc.ebs_optimized,
-      associate_public_ip_address: lc.associate_public_ip_address,
-      placement_tenancy: lc.placement_tenancy
-   }
+   opt = {launch_configuration_name: lcName}
+   if (lc.image_id)
+      opt[:image_id] = lc.image_id
+   end
+   if (lc.key_name )
+      opt[:key_name] = lc.key_name
+   end
+   if (lc.security_groups )
+      opt[:security_groups] = lc.security_groups
+   end
+   if (lc.classic_link_vpc_id )
+      opt[:classic_link_vpc_id] = lc.classic_link_vpc_id
+   end
+   if (lc.classic_link_vpc_security_groups )
+      opt[:classic_link_vpc_security_groups] = lc.classic_link_vpc_security_groups
+   end
+   if (lc.user_data )
+      opt[:user_data] = lc.user_data
+   end
+   if (lc.instance_type )
+      opt[:instance_type] = lc.instance_type
+   end
+   if (lc.kernel_id )
+      opt[:kernel_id] = lc.kernel_id
+   end
+   if (lc.ramdisk_id )
+      opt[:ramdisk_id] = lc.ramdisk_id
+   end
+   if (lc.block_device_mappings )
+      opt[:block_device_mappings] = lc.block_device_mappings
+   end
+   opt[:instance_monitoring] = {enabled: false}
+   if (lc.instance_monitoring.enabled )
+      opt[:instance_monitoring][:enabled] = true
+   end
+   if (lc.spot_price )
+      opt[:spot_price] = lc.spot_price
+   end
+   if (lc.iam_instance_profile )
+      opt[:iam_instance_profile] = lc.iam_instance_profile
+   end
+   if (lc.ebs_optimized )
+      opt[:ebs_optimized] = lc.ebs_optimized
+   end
+   if (lc.associate_public_ip_address )
+      opt[:associate_public_ip_address] = lc.associate_public_ip_address
+   end
+   if (lc.placement_tenancy )
+      opt[:placement_tenancy] = lc.placement_tenancy
+   end
+
    return opt
 end
 
